5a6523c398acc3be706675d2eac0e3153eb10ef6
Baudouin <64631542+scriptor-pro@users.noreply.github.com>
2025-06-23 18:37:06 +0200

    Update quartz.config.ts fr-BE vers fr-FR

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "fr-BE",
+    locale: "fr-FR",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
